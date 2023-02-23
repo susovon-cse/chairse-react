9d27e4fe455cd91ee3bffdb56d519c34a5809e52
Susovon Mondal <susovonmondal45@gmail.com>
2023-02-23 20:29:57 +0530

    update1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Footer from './component/common/Footer';
+import Navbar from './component/common/Navbar';
+import Home from './component/core/Home';
+
+function App() {
+  return (
+    <>
+     <Navbar/>
+     <Home/>
+     <Footer/>
+    </>
+  );
+}
+
+export default App;
